2407c5279d83fb3ae55e89842bf3a9e3e6b1df0b
bipul <bipul.kumar@voltmoney.in>
2025-03-16 16:03:35 +0530

    code

--- src/BinarySearch/BinarySearch.java
+++ src/BinarySearch/BinarySearch.java
@@ -1,5 +1,7 @@
 package BinarySearch;
 
+import java.util.Arrays;
+
 public class BinarySearch {
     private int[] arr;
     BinarySearch(int[] arr){
@@ -96,6 +98,30 @@ public class BinarySearch {
         }
         return left;
     }
+    public  int[]  findfloorAndCeil(int[] nums,int target) {
+        int left = 0, right = nums.length - 1;
+        int firstOccurence=-1;
+        int[] floorAndCeil=new int[2];
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid]==target){
+                if(mid==0){
+                    return new int[]{-1,nums[mid+1]};
+                }
+                else if(mid==nums.length-1){
+                    return new int[]{nums[mid-1],-1};
+                }
+                 return   new int[]{nums[mid-1],nums[mid+1]};
+            }
+            else if(nums[mid]<target){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return new int[]{-1,-1};
+    }
     public static void main(String[] args) {
         int nums[] = { 2, 5, 6, 8, 9, 10 };
         BinarySearch binarySearch= new BinarySearch(nums);
@@ -109,7 +135,7 @@ public class BinarySearch {
         System.out.println(binarySearch.noOfRotation(circularrotatedArray));
 
 
-        int Occurence[] ={1,2,3,3,3,4,4,5,6,6,6,7,8,9};
+        int Ocurence[] ={1,2,3,3,3,4,4,5,6,6,6,7,8,9};
         System.out.println(binarySearch.firstOccurence(Ocurence,2)==1);
         System.out.println(binarySearch.firstOccurence(Ocurence,3)==2);
         System.out.println(binarySearch.firstOccurence(Ocurence,4)==5);
@@ -124,14 +150,18 @@ public class BinarySearch {
         System.out.println(binarySearch.lastOccurence(Ocurence,5)==7);
         System.out.println(binarySearch.lastOccurence(Ocurence,6)==10);
 
-        System.out.println(binarySearch.findSmallestMissing(new int[]{0,1,2,3,4,5,6,8,9,10})==7);
-        System.out.println(binarySearch.findSmallestMissing(new int[]{0,1,3,4,5,6,7,8,9,10})==2);
-        System.out.println(binarySearch.findSmallestMissing(new int[]{0,2,3,4,5,6,7,8,9,10})==1);
-        System.out.println(binarySearch.findSmallestMissing(new int[]{1,2,3,4,5,6,7,8,9,10})==0);
+
+
+        System.out.println(Arrays.toString(binarySearch.findfloorAndCeil(new int[]{0,1,2,3,4,5,6,8,9,10},3)));
+        System.out.println(Arrays.toString(binarySearch.findfloorAndCeil(new int[]{0,1,3,4,5,6,7,8,9,10},5)));
+        System.out.println(Arrays.toString(binarySearch.findfloorAndCeil(new int[]{0,2,3,4,5,6,7,8,9,10},7)));
+        System.out.println(Arrays.toString(binarySearch.findfloorAndCeil(new int[]{1,2,3,4,5,6,7,8,9,10},8)));
+        System.out.println(Arrays.toString(binarySearch.findfloorAndCeil(new int[]{1,2,3,4,5,6,7,8,9,10},1)));
+        System.out.println(Arrays.toString(binarySearch.findfloorAndCeil(new int[]{1,2,3,4,5,6,7,8,9,10},10)));
 
 
 
-        //
+
 
     }
 }
